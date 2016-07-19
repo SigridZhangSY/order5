94521af3a19ed86fd0081245c4fe3955c9cba2cf
SigridZhangSY <sigridzhang0603@163.com>
2016-07-19 23:52:17 +0800

    return 400 when create user with name is empty

--- src/main/java/com/thoughtworks/ketsu/web/UsersResource.java
+++ src/main/java/com/thoughtworks/ketsu/web/UsersResource.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.ketsu.web;
 
+import com.thoughtworks.ketsu.infrastructure.core.User;
 import com.thoughtworks.ketsu.infrastructure.core.UserRepository;
 import com.thoughtworks.ketsu.infrastructure.records.UserRecord;
+import com.thoughtworks.ketsu.web.exception.InvalidParameterException;
 import com.thoughtworks.ketsu.web.jersey.Routes;
 
 import javax.ws.rs.Consumes;
@@ -10,7 +12,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Path("users")
 public class UsersResource {
@@ -20,6 +25,14 @@ public class UsersResource {
     public Response createUser(Map<String, Object> info,
                                @Context Routes routes,
                                @Context UserRepository userRepository){
+        List<String> invalidParameter = new ArrayList<>();
+        if(info.getOrDefault("name", "").toString().trim().isEmpty())
+            invalidParameter.add("name");
+        if(invalidParameter.size() > 0)
+            throw new InvalidParameterException(invalidParameter);
+        Optional<User> user = userRepository.findUserByName(String.valueOf(info.get("name")));
+        if(user.isPresent())
+            throw new InvalidParameterException("user has existed");
         return Response.created(routes.userUri(userRepository.createUser(info))).build();
     }
 }
--- src/test/java/com/thoughtworks/ketsu/resources/UsersResourceTest.java
+++ src/test/java/com/thoughtworks/ketsu/resources/UsersResourceTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.ketsu.resources;
 
+import com.thoughtworks.ketsu.infrastructure.core.UserRepository;
 import com.thoughtworks.ketsu.support.ApiSupport;
 import com.thoughtworks.ketsu.support.ApiTestRunner;
 import com.thoughtworks.ketsu.support.TestHelper;
@@ -8,8 +9,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import static org.hamcrest.CoreMatchers.endsWith;
@@ -18,6 +23,9 @@ import static org.junit.Assert.assertThat;
 
 @RunWith(ApiTestRunner.class)
 public class UsersResourceTest extends ApiSupport{
+    @Inject
+    UserRepository userRepository;
+
     @Before
     public void setUp() throws Exception {
         super.setUp();
@@ -28,5 +36,22 @@ public class UsersResourceTest extends ApiSupport{
         Response post = post("users", TestHelper.userMap("John"));
         assertThat(post.getStatus(), is(HttpStatus.CREATED_201.getStatusCode()));
         assertThat(Pattern.matches(".*?/users/[0-9-]*", post.getLocation().toASCIIString()), is(true));
+    }
+
+    @Test
+    public void should_return_400_when_create_user_with_name_exists(){
+        userRepository.createUser(TestHelper.userMap("John"));
+        Response post = post("users", TestHelper.userMap("John"));
+        assertThat(post.getStatus(), is(HttpStatus.BAD_REQUEST_400.getStatusCode()));
+    }
+
+    @Test
+    public void should_return_400_when_create_user_with_name_is_null(){
+
+        Response post = post("users", new HashMap<String, Object>());
+        assertThat(post.getStatus(), is(HttpStatus.BAD_REQUEST_400.getStatusCode()));
+        final List<Map<String, Object>> res = post.readEntity(List.class);
+        assertThat(res.size(), is(1));
+        assertThat(res.get(0).get("message"), is("name can not be empty"));
     }
 }
