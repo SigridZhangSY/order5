6f384dfd0d7a8994fa3618bb0ecb0deb8fa69d93
SigridZhangSY <sigridzhang0603@163.com>
2016-07-19 17:10:49 +0800

    return 201 when create product

---
+++ src/main/java/com/thoughtworks/ketsu/web/ProductResource.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.ketsu.web;
+
+import com.thoughtworks.ketsu.infrastructure.records.ProductRecord;
+import com.thoughtworks.ketsu.web.jersey.Routes;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.container.ResourceContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("products")
+public class ProductResource {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createProduct(@Context Routes routes){
+        return Response.status(201).build();
+    }
+}
